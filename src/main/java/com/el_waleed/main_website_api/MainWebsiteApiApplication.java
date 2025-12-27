2a2a7259de2cca72a21baa0bb6f92d3c2710de94
mo <mohamedaamer867@gmail.com>
2025-12-27 01:10:06 +0100

    First Commit

---
+++ src/main/java/com/el_waleed/main_website_api/MainWebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.el_waleed.main_website_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainWebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainWebsiteApiApplication.class, args);
+	}
+
+}
