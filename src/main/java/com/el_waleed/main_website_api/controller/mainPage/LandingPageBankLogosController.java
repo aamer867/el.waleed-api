581b1d10efeb86d4d69c0d6e1e3ae345a729c440
mo <mohamedaamer867@gmail.com>
2026-03-09 22:49:20 +0100

    Start Landing Page Banks Logo Section

---
+++ src/main/java/com/el_waleed/main_website_api/controller/mainPage/LandingPageBankLogosController.java
@@ -0,0 +1,4 @@
+package com.el_waleed.main_website_api.controller.mainPage;
+
+public class LandingPageBankLogosController {
+}
---
+++ src/main/java/com/el_waleed/main_website_api/dto/BankData.java
@@ -0,0 +1,16 @@
+package com.el_waleed.main_website_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class BankData {
+    private String name;
+    @JsonProperty("image_url")
+    private String imageUrl;
+    @JsonProperty("link")
+    private String bankLink;
+    @JsonIgnore
+    private ImageHandler image;
+}
